c80c9765594a48863b2dcb55f745c5963f8a2290
Alikhard <alikhard@gmail.com>
2016-04-22 03:42:35 -0400

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/gohard/MainActivity2.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.gohard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+}
