6a7eb596be3898bc0baf58ae997a16e88152dc7f
Romiel <alikhard@gmail.com>
2016-10-04 00:32:11 -0400

    AllActivities[Alert/Toast]

--- app/src/main/java/com/bignerdranch/android/gohard/NewSessionActivity.java
+++ app/src/main/java/com/bignerdranch/android/gohard/NewSessionActivity.java
@@ -1,15 +1,18 @@
 package com.bignerdranch.android.gohard;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class NewSessionActivity extends AppCompatActivity {
 
     Button submitBtn;
-
+    Button btLogOff;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +27,35 @@ public class NewSessionActivity extends AppCompatActivity {
             }
         });
 
+        btLogOff = (Button)findViewById(R.id.btLogOff);
+        btLogOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                diag();
+            }
+
+        });
+
+    }
+
+    public void diag() {
+
+        new AlertDialog.Builder(this)
+                .setTitle("Exit")
+                .setMessage("Do You Want To Log Off?")
+                .setNegativeButton("No",null)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface diag, int which) {
+                        showToast();
+                        Intent in = new Intent(NewSessionActivity.this, LoginActivity.class);
+                        startActivity(in);
+                    }
+
+                }).create().show();
+    }
+
+    public void showToast() {
+        Toast.makeText(NewSessionActivity.this, "Logging You Off", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/bignerdranch/android/gohard/PersonActivity.java
+++ app/src/main/java/com/bignerdranch/android/gohard/PersonActivity.java
@@ -1,20 +1,24 @@
 package com.bignerdranch.android.gohard;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PersonActivity extends AppCompatActivity {
 
     CheckBox ckB;
     TextView seInfo;
+    Button btLogOff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person);
@@ -33,6 +37,36 @@ public class PersonActivity extends AppCompatActivity {
             }
         });
 
+        btLogOff = (Button)findViewById(R.id.btLogOff);
+        btLogOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                diag();
+            }
+
+        });
+
+    }
+
+    public void diag() {
+
+        new AlertDialog.Builder(this)
+                .setTitle("Exit")
+                .setMessage("Do You Want To Log Off?")
+                .setNegativeButton("No",null)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface diag, int which) {
+                        showToast();
+                        Intent in = new Intent(PersonActivity.this, LoginActivity.class);
+                        startActivity(in);
+                    }
+
+                }).create().show();
+    }
+
+    public void showToast() {
+        Toast.makeText(PersonActivity.this, "Logging You Off", Toast.LENGTH_SHORT).show();
     }
 
 
